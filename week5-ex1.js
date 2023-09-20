86f1d7bfa449a4a87a43069e6ef9183f0790292c
steffengroenandersen <steffengroenandersen@gmail.com>
2023-09-20 08:09:15 +0200

    Add exercise 2

--- week5-ex1.js
+++ week5-ex1.js
@@ -1,21 +1,13 @@
 /*
-    Event Handler.. on click
-    activate method, that takes value from input
-    and creates a new string https://jsonplaceholder.typicode.com/users/INPUT
-    creates innerHTML of p tag?
+    Exercise 1
 */
 
 const URL = "https://jsonplaceholder.typicode.com/users/";
 
-document.querySelector("#show-user").innerHTML = "User X!";
-
-
 document.querySelector("#get-user-button").onclick = (evt) => {
     evt.preventDefault();
-    console.log("User Button was Clicked!");
+    console.log("Get User Button was Clicked!");
     showUser();
-
- 
 }
 
 // Function that display the user inside innerHTML
@@ -23,7 +15,17 @@ async function showUser(){
     const userId = document.querySelector("#user-id-input").value;
     const userObject = await fetchUserById(userId);
     console.log(userObject);
+    const userHTML = convertUserObjectToString(userObject);
+    const showUserHTML = document.querySelector("#show-user");
+    showUserHTML.innerHTML = userHTML;
+}
 
+// Function that transform userObject toString
+function convertUserObjectToString(userObject){
+    return  "<p>" + "Name: " + userObject.name +  "<br />" + "Email: " + userObject.email + "</p>" +
+            "<p>" + "Address: " + "<br />" + 
+            "City: " + userObject.address.city + "<br />" +
+            "Street: " + userObject.address.street + "</p>";
 }
 
 // Function that fetches one user by id
@@ -32,8 +34,41 @@ async function fetchUserById(userId){
     return await response.json();
 }
 
-// TODO: Function that transform userObject toString
+/*
 
+    Exercise 2
+
+*/
+
+// Add event listener
+document.querySelector("#get-all-users-button").onclick = (evt) => {
+    evt.preventDefault();
+    console.log("Get All Users Button Clicked");
+    showAllUsers();
+}
+
+// Function that displays all users in table
+async function showAllUsers(){
+    // fetch all users from api
+    const allUsers = await fetchAllUsers();
+    
+    // convert array to proper html syntax
+    const allUsersHTML = allUsers.map(u => `<tr><td>${u.name}</td><td>${u.email}</td></tr>`).join("");
+    
+    // change innerHTML
+    const userTableData = document.querySelector("#tbody");
+    userTableData.innerHTML = allUsersHTML;
+}
+
+// function that fetches from db
+async function fetchAllUsers(){
+
+    const response = await fetch(URL)
+        .then(res => res.json())
+        .then(data => data);
+    console.log(response);
+    return await response;
+}
 
 /*
 
